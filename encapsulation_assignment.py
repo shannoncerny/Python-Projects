5bbba55267c3abddae2d718385b7c616f8258dca
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-01-26 15:09:10 -0600

    Updated Encapsulation Assignment

--- encapsulation_assignment.py
+++ encapsulation_assignment.py
@@ -2,23 +2,16 @@
 class Protected:
     def __init__(self):
         self._protectedVar = 0
-    obj = Protected()
-    obj._protectedVar = 11
-    # output will be 11
-    print(obj._protectedVar)
+        self.__privateVar = 23 
 
-# PRIVATE VARIABLE
-class Private:
-    def __init__(self):
-        self.__privateVar = 23
-
+    # PRIVATE VARIABLE
     def getPrivate(self):
         print(self.__privateVar)
 
-    def setPrivate(self, private):
+    def setPrivate(self,private):
         self.__privateVar = private
 
 obj = Protected()
-obj.getPrivate()
-obj.setPrivate(47) # give privateVar a new value
+obj.getPrivate() # retrieves value stored in privateVar
+obj.setPrivate(47) # privateVar new value
 obj.getPrivate() # retrieves new value stored in privateVar
